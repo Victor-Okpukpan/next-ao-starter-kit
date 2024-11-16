50f3387e5078881030bd57161cd25a107c667d8d
Victor Okpukpan <vokpukpan@gmail.com>
2024-11-16 12:10:09 +0100

    chore: change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AO + Next",
+  title: "Next.js + AO",
   description: "Generated with Next AO Starter Kit.",
 };
 
